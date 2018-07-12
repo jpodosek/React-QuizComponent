525952e9e6560761be18ac42a7a820e97692d259
Jon Podosek <jon.podosek@libertymutual.com>
2018-07-12 08:41:53 -0700

    Completed Module 6, although tests are still failing.

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -3,6 +3,15 @@ import QuizQuestionButton from './QuizQuestionButton.js';
 
 
 class QuizQuestion extends Component {
+    
+    constructor(props) {
+            super(props)
+    }
+    handleClick (buttonText) {
+        if (buttonText === this.props.quiz_question.answer) {
+            this.props.showNextQuestionHandler();
+        }
+    }
     render() {
         return (
             <main>
@@ -11,7 +20,9 @@ class QuizQuestion extends Component {
             </section>
             <section className="buttons">
               <ul>
-                <QuizQuestionButton button_text = {this.props.quiz_question.answer_options[0]} />
+                   {this.props.quiz_question.answer_options.map((answer_option, index) => {
+                       return <QuizQuestionButton key={index} button_text={answer_option}  clickHandler={this.handleClick.bind(this)}/>
+                   })}
             </ul>
             </section>
           </main>
